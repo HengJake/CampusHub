b5fb263355fff0b7167e34bdfa270d980806a859
Heng Jun Kai <hengjunkai@gmail.com>
2025-05-25 00:22:22 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-module.exports = (req, res) => {
+app.get('/', (req, res) => {
   res.send("Server is running");
-};
+});
+
+app.listen(5000, () => {
+  console.log('Server started on PORT 5000');
+});
