e6d33651da6bdcbbe74be0ac25ba8ff8fb2c8551
Heng Jun Kai <hengjunkai@gmail.com>
2025-05-25 00:23:43 +0800

    nvm

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => {
-  res.send("Server is running");
+app.use("/", (req, res) => {
+    res.send("Server is running");
 });
 
-app.listen(5000, () => {
-  console.log('Server started on PORT 5000');
-});
+app.listen(5000, console.log("Server started on PORT 5000"));
